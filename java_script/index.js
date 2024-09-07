e97961fbd8758568af4a74ed3a41df662adf64ca
dhruvdk3 <dhruvkumawat333@gmail.com>
2024-09-07 23:21:02 +0530

    JS

--- java_script/index.js
+++ java_script/index.js
@@ -1,2 +1,12 @@
 console.log("Hello");
-alert("its a trial");
+let a = 5;
+var b = 5;
+const c = 12;
+console.log(5&&true);
+console.log(5&&false);
+console.log(true&&5);
+console.log(false&&5);
+console.log(5|2);
+for (let index = 0; index < 5; index++) {
+    console.log(index)
+}
