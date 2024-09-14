8f42be369a558c0ea7e32f5ef08f98d84a159b4a
dhruvdk3 <dhruvkumawat333@gmail.com>
2024-09-15 00:26:58 +0530

    js

--- java_script/index.js
+++ java_script/index.js
@@ -60,11 +60,18 @@ date = new Date(2002, 4, 30, 7);
 date.setFullYear(2031);
 console.log(date)
 
-let l = [1,3,4,5, 5];
+let l = [1,3,4,5];
+console.log(l);
 l.push(6);
 l.unshift(0);
 l.splice(2,0,2);
 console.log(l);
+
+l.pop();
+l.shift();
+l.splice(1,1);
+console.log(l);
+
 console.log(l.indexOf(1,5));
 console.log(5 in l);
 
@@ -78,3 +85,7 @@ let details = [
 let detail = details.find(detail=>detail.Name == "Dhruv");
 
 console.log(detail);
+
+x1 = [1,2,3];
+x2 = [4,5,6];
+console.log(x1.concat(x2).slice(2,5));
