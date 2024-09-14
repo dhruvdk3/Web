0038527cbe9ab45b09fc9a74ecc8ff9ddbaa0013
dhruvdk3 <dhruvkumawat333@gmail.com>
2024-09-14 23:27:10 +0530

    Arr

--- java_script/index.js
+++ java_script/index.js
@@ -49,10 +49,34 @@ console.log(b.value);
 if("lengthi" in rectangle1){
     console.log(1);
 }
-let z = Object.assign({}, x);
-// for(let key in x){
-//     z[key] = x[key];
-// }
-
+let z = Object.assign({hehe:4}, x);
+// let z = {...x};
 z.length++;
 console.log(z);
+
+let date = new Date();
+date = new Date("May 2002 18:16");
+date = new Date(2002, 4, 30, 7);
+date.setFullYear(2031);
+console.log(date)
+
+let l = [1,3,4,5, 5];
+l.push(6);
+l.unshift(0);
+l.splice(2,0,2);
+console.log(l);
+console.log(l.indexOf(1,5));
+console.log(5 in l);
+
+let details = [
+    {Name: "Dhruv", age:22},
+    {Name: "hehe", age:25},
+    {Name: "hehe2", age:27},
+];
+
+
+let detail = details.find(function(detail){
+    return detail.Name == "Dhruv";
+});
+
+console.log(detail);
