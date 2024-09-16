a3d3c304a157eeb7bb99fec61da04e39f6373075
dhruvdk3 <dhruvkumawat333@gmail.com>
2024-09-16 12:56:46 +0530

    arguments

--- java_script/index.js
+++ java_script/index.js
@@ -100,8 +100,8 @@
 // console.log(b);
 
 function sum(a,b){
-    console.log(arguments)
-    return a+b;
+    let total = 0;
+    for (let i of arguments) total+=i;
+    return total;
 }
-
 console.log(sum(1,2,3,4,5,6));
